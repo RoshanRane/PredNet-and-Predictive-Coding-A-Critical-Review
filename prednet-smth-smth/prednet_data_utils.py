106ecc5c801be5377fff837198aaac884bf6b111
Vageesh.Saxena <saxena@uni-potsdam.de>
2019-01-09 01:00:29 +0100

    Work in progress for smth smth prednet version

---
+++ prednet-smth-smth/prednet_data_utils.py
@@ -0,0 +1,326 @@
+########################################## Importing libraries #########################################################
+import os
+import glob
+import sys
+
+import threading
+
+import numpy as np
+import pandas as pd
+
+from matplotlib import pyplot as plt
+from skimage import io, transform
+from PIL import Image as pil_image
+
+from keras import backend as K
+from keras_preprocessing.image import get_keras_submodule, load_img, img_to_array
+########################################################################################################################
+
+
+try:
+    IteratorType = get_keras_submodule('utils').Sequence
+except ImportError:
+    IteratorType = object
+
+
+class myIterator(IteratorType):
+    """
+    Description : Base class for image data iterators from keras_preprocessing.image
+    modified for our multiple-crop size purpose.
+
+    # Arguments
+        n: Integer, total number of samples in the dataset to loop over.
+        batch_size: Integer, size of a batch.
+        shuffle: Boolean, whether to shuffle the data between epochs.
+        seed: Random seeding for data shuffling.
+    """
+
+    def __init__(self, n, batch_size, shuffle, seed, grps_boundary=None):
+        '''The grps_boundaries is used to define sub-groups within the whole sample group.'''
+        self.n = n
+        self.batch_size = batch_size
+
+        assert (grps_boundary is None) or (
+                    grps_boundary <= self.n), "The index 'grps_boundary' must be less than n (number of samples)"
+        self.boundary = grps_boundary
+        if self.boundary is None:
+            self.total_batches = self.n // self.batch_size
+        else:
+            self.total_batches = ((self.boundary // self.batch_size) + (self.n - self.boundary) // self.batch_size)
+
+        self.seed = seed
+        self.shuffle = shuffle
+        self.batch_index = 0
+        self.total_batches_seen = 0
+        self.lock = threading.Lock()
+        self.index_array = None
+        self.index_generator = self._flow_index()
+
+    def _set_index_array(self):
+        self.index_array = np.arange(self.n)
+        if self.shuffle:
+            if self.boundary is None:
+                self.index_array = np.random.choice(
+                    self.index_array
+                    , size=(self.total_batches, self.batch_size)
+                    , replace=False
+                )
+            else:
+                # when there are 2 sub-grps shuffle then seperately and then create their batches at indexes
+                self.index_array = np.append(
+                    np.random.choice(
+                        self.index_array[:self.boundary]
+                        , size=(self.boundary // self.batch_size, self.batch_size)
+                        , replace=False)
+                    , np.random.choice(
+                        self.index_array[self.boundary:]
+                        , size=((self.n - self.boundary) // self.batch_size, self.batch_size)
+                        , replace=False)
+                    , axis=0)
+                np.random.shuffle(self.index_array)
+
+    #         print(self.index_array)
+
+    def __getitem__(self, idx):
+        if idx >= len(self):
+            raise ValueError('Asked to retrieve element {idx}, '
+                             'but the Sequence '
+                             'has length {length}'.format(idx=idx,
+                                                          length=len(self)))
+        if self.seed is not None:
+            np.random.seed(self.seed + self.total_batches_seen)
+        self.total_batches_seen += 1
+
+        # start_index = self.batch_size *
+        # end_index = self.batch_size * (idx + 1)
+        # #handle boundary condition when there will be mixed indexes from both grps
+        # if((start_index < self.boundary) and (end_index > self.boundary)):
+        #   randomly select the previous index which is in the first group or next index in the next grps
+        #   start_index = np.random.choice([self.batch_size*(idx-1), end_index])
+        #                               p left at default 50%-50% for simplicity
+        #   end_index = start_index + self.batch_size
+
+        # index_array = self.index_array[start_index:end_index]
+        index_array = self.index_array[idx]
+        return self._get_batches_of_transformed_samples(index_array)
+
+    def __len__(self):
+        return self.total_batches
+
+        # check if epoch_end has been reached. If yes, reset the iterator and shuffle the data
+
+    def on_epoch_end(self):
+        self._set_index_array()
+
+    def reset(self):
+        self.batch_index = 0
+
+    def _flow_index(self):
+        # Ensure self.batch_index is 0.
+        self.reset()
+        while 1:
+            # print(self.batch_index)
+            if self.seed is not None:
+                np.random.seed(self.seed + self.total_batches_seen)
+            if self.batch_index == 0:
+                self._set_index_array()
+
+            # current_index = (self.batch_index * self.batch_size) % self.n
+            # end_index = current_index + self.batch_size
+
+            # handle boundary condition when there will be mixed indexes from both grps
+            # if((current_index < self.boundary) and (end_index > self.boundary)):
+            #        randomly select either the previous index (which is in the first group)
+            #        or the next index (inside the second grp)
+            #        current_index = np.random.choice([(current_index-self.batch_size), end_index])
+            #                                             p left at default 50%-50% for simplicity
+            # end_index = current_index + self.batch_size
+            cur_batch_index = self.batch_index
+            # increment batch index for next iter
+            if self.batch_index < (self.total_batches - 1):
+                self.batch_index += 1
+            else:
+                self.batch_index = 0
+
+            self.total_batches_seen += 1
+
+            yield self.index_array[cur_batch_index]
+
+    def __iter__(self):
+        # Needed if we want to do something like:
+        # for x, y in data_gen.flow(...):
+        return self
+
+    def __next__(self, *args, **kwargs):
+        #         return next(self.index_generator)
+        return self.next(*args, **kwargs)
+
+    def _get_batches_of_transformed_samples(self, index_array):
+        """Gets a batch of transformed samples.
+        # Arguments
+            index_array: Array of sample indices to include in batch.
+        # Returns
+            A batch of transformed samples.
+        """
+        raise NotImplementedError
+
+
+class SmthSmthSequenceGenerator(myIterator):
+    '''
+    Data generator that creates batched sequences from the smth-smth dataset for input into PredNet.
+    info: to generate the data_csv, run the extract_20bn.py script first on the raw smth-smth videos.
+    '''
+
+    def __init__(self, data_csv_path, split, nframes
+                 , batch_size=8
+                 , shuffle=True, seed=None
+                 , output_mode='error'
+                 , nframes_selection_mode="smth-smth-baseline-method"
+                 #                  , N_seq=None
+                 #                  , img_interpolation='nearest'
+                 , data_format=K.image_data_format()
+                 ):
+
+        if seed is not None:
+            np.random.seed(seed)
+
+        self.df = pd.read_csv(data_csv_path)
+        assert split in set(self.df['split']), "split should be one of {}".format(set(df['split']))
+        # select the split subset of the data and sort them by the crop group
+        self.df = self.df[self.df['split'] == split].sort_values(by=['crop_group']).reset_index(drop=True)
+        self.grp1_boundary = len(self.df[self.df['crop_group'] == 1])
+
+        assert output_mode in {'error', 'prediction'}, 'output_mode must be in {error, prediction}'
+        self.output_mode = output_mode
+        self.batch_size = batch_size
+        self.nframes = nframes
+        self.shuffle = shuffle
+        self.seed = seed
+
+        if (data_format != 'channels_last'):
+            raise NotImplementedError(
+                "Only 'channels_last' data_format is currently supported by this class.'channels_first' is not supported")
+
+        assert nframes_selection_mode in {
+            "smth-smth-baseline-method", "reject-extremes", "dynamic-fps"
+        }, 'nframes_selection_mode must be one of {"smth-smth-baseline-method", "reject-extremes","dynamic-fps"}'
+        self.nframes_selection_mode = nframes_selection_mode
+
+        super(SmthSmthSequenceGenerator, self).__init__(n=len(self.df),
+                                                batch_size=batch_size,
+                                                shuffle=shuffle,
+                                                seed=seed,
+                                                grps_boundary=self.grp1_boundary)
+
+    def _get_batches_of_transformed_samples(self, index_array):
+
+        # check which crop size group is expected...preprocessing (1)
+        if (all(index_array < self.grp1_boundary)):
+            target_im_size = (128, 160)
+        elif (all(index_array >= self.grp1_boundary)):
+            target_im_size = (128, 224)
+        else:
+            raise ValueError(
+                "index_array {} contains a mix of both groups.They should all be either less than {} or greater than {}".format(
+                    index_array, self.grp1_boundary, self.grp1_boundary - 1))
+
+        batch_x = np.empty(((len(index_array),) + (self.nframes,) + target_im_size + (3,))
+                           , dtype=np.float32)
+
+        for i, idx in enumerate(index_array):
+            # read the video dir
+            vid_dir = self.df.loc[idx, 'path']
+            batch_x[i] = self.fetch_and_preprocess(vid_dir, target_im_size)
+        return batch_x
+
+    def fetch_and_preprocess(self, vid_dir, target_im_size):
+
+        frames = sorted(glob.glob(vid_dir + "/*.png"))
+        total_frames = len(frames)
+        # select exactly 'nframes' from each video dir... preprocessing (4)
+        if (self.nframes_selection_mode == "smth-smth-baseline-method"):
+            if (total_frames > self.nframes):
+                # sample the start frame using a binomial distribution highest probability at the center of the video
+                start_frame_idx = np.random.binomial((total_frames - self.nframes), p=0.5)
+                frames_out = frames[start_frame_idx: start_frame_idx + self.nframes]
+            else:
+                # replicate the first frame and last frame at the ends to match self.nframes
+                replicate_cnt_start = (self.nframes - total_frames) // 2
+                replicate_cnt_end = (self.nframes - (total_frames + replicate_cnt_start))
+                frames_out = [frames[0]] * (replicate_cnt_start) + frames + [frames[-1]] * (replicate_cnt_end)
+        else:
+            raise NotImplementedError
+
+        X = np.empty(((self.nframes,) + target_im_size + (3,)), dtype=np.float32)
+
+        for i, frame in enumerate(frames_out):
+            X[i] = self.load_img(frame, target_size=target_im_size)
+        X = self.preprocess(X)
+        return X
+
+    def load_img(self, img_dir, target_size):
+        #       _PIL_INTERPOLATION_METHODS = {
+        #         'nearest': pil_image.NEAREST,
+        #         'bilinear': pil_image.BILINEAR,
+        #         'bicubic': pil_image.BICUBIC,
+        # }
+        im = pil_image.open(img_dir)
+        w, h = im.size
+        # resize but maintain the aspect ratio
+        w_same_aspect = int((target_size[0] / h) * w)
+        im = im.resize((target_size[0], w_same_aspect), pil_image.ANTIALIAS)
+        # center crop the width to target_size
+        w_crop = (im.size[1] - target_size[1]) // 2
+        im = im.crop((0, w_crop, im.size[0], target_size[1] + w_crop))
+        im_arr = np.asarray(im, dtype=np.float32)
+        im_arr = np.moveaxis(im_arr, 0, 1)
+        return im_arr
+
+    def preprocess(self, X):
+        #         self.image_data_generator = ImageDataGenerator(
+        #                                     featurewise_center=False
+        #                                      , samplewise_center=False
+        #                                      , featurewise_std_normalization=False
+        #                                      , samplewise_std_normalization=False
+        #                                      , zca_whitening=False
+        #                                      , zca_epsilon=1e-06
+        #                                      , rotation_range=0
+        #                                      , width_shift_range=0.0
+        #                                      , height_shift_range=0.0
+        #                                      , brightness_range=None
+        #                                      , shear_range=0.0
+        #                                      , zoom_range=0.0
+        #                                      , channel_shift_range=0.0
+        #                                      , fill_mode='nearest'
+        #                                      , cval=0.0
+        #                                      , horizontal_flip=True
+        #                                      , vertical_flip=False
+        #                                      , rescale=1./255
+        #                                      , preprocessing_function=None
+        #                                      , data_format='channels_last'
+        #                                      , validation_split=0.0
+        #                                      , dtype='float32'
+        #                                     )
+        #         X = self.image_data_generator.standardize(X) # standardize to range [0,1]... preprocessing 3
+        #         params = {'flip_horizontal':True}
+        #         X = self.image_data_generator.apply_transform(X, params)
+        return X / 255
+
+    """def create_all(self):
+        X_all = np.zeros((self.N_sequences, self.nt) + (160, 224), np.float32)
+        for i, idx in enumerate(self.possible_starts):
+            X_all[i] = self.preprocess(self.X[idx:idx + self.nt])
+        return X_all
+    """
+
+    def next(self):
+        """For python 2.x. # Returns  The next batch.
+        info : function taken directly from keras_preprocessing.DataFrameIterator class
+        """
+        # Keeps under lock only the mechanism which advances
+        # the indexing of each batch.
+        with self.lock:
+            index_array = next(self.index_generator)
+        # The transformation of images is not under thread lock
+        # so it can be done in parallel
+        return self._get_batches_of_transformed_samples(index_array)
---
+++ prednet-smth-smth/preprocess_data.py
@@ -0,0 +1,168 @@
+########################################## Importing libraries #########################################################
+import os
+import sys
+import glob
+from time import sleep
+
+import json
+
+from tqdm import tqdm
+
+import pandas as pd
+########################################################################################################################
+
+
+def split_data(data_dir, train_json_name="something-something-v2-train.json",
+               test_json_name="something-something-v2-test.json",
+               val_json_name="something-something-v2-validation.json",
+               split_flag=True):
+    '''
+    Input : Directory of the data to be processed (dtype: String)
+    Description : Splits the videos into train, val, test folders.
+                  Turn off the split_flag to disable the functionality.
+    Convention : data_dir should contain the raw videos in a 'raw' folder and the train, test and val json files.
+    '''
+    print("Pre-processing step 1: Splitting videos into train, test, and val datasets.")
+
+    if split_flag:
+        # Getting the files
+        train_json = os.path.join(data_dir, train_json_name)
+        test_json = os.path.join(data_dir, test_json_name)
+        val_json = os.path.join(data_dir, val_json_name)
+
+        # Checking if the file exists
+        assert os.path.isfile(train_json)
+        assert os.path.isfile(test_json)
+        assert os.path.isfile(val_json)
+
+        # Dividing files into train, test, and validation dataset
+        train_data = json.load(train_json)
+        train_data = [entries["id"] for entries in train_data]
+
+        test_data = json.load(test_json)
+        test_data = [entries["id"] for entries in test_data]
+
+        val_data = json.load(val_json)
+        val_data = [entries["id"] for entries in val_data]
+
+        # Creating the necessary directories
+        os.system("mkdir -p {}/preprocessed".format(data_dir))
+        os.system("mkdir -p {}/preprocessed/train".format(data_dir))
+        os.system("mkdir -p {}/preprocessed/test".format(data_dir))
+        os.system("mkdir -p {}/preprocessed/val".format(data_dir))
+
+        videos = [video for video in os.listdir(os.path.join(data_dir, "raw"))]
+        print("Total number of videos:", len(videos))
+
+        with tqdm(total=100, file=sys.stdout) as pbar:
+            for files in tqdm(videos):
+                ids = files.split(".")[0]
+                if ids in test_data:
+                    os.system("cp -u {} {}".format(os.path.join(data_dir, "raw", files),
+                                                   os.path.join(data_dir, "preprocessed/test/", files)))
+                elif ids in train_data:
+                    os.system("cp -u {} {}".format(os.path.join(data_dir, "raw", files),
+                                                   os.path.join(data_dir, "preprocessed/train/", files)))
+                elif ids in val_data:
+                    os.system("cp -u {} {}".format(os.path.join(data_dir, "raw", files),
+                                                   os.path.join(data_dir, "preprocessed/val/", files)))
+                else:
+                    print("{} is listed neither in test, train nor val data.".format(files))
+                pbar.update(1)
+                sleep(1)
+    else:
+        pass
+
+
+def extract_videos(raw_vids, dest_dir, fps=None, extract_flag=True):
+    '''
+    Input : raw_vids -
+            dest_dir - The dir in which the final extracted and processed videos will be placed. (dtype: String)
+    Description : Converts .webm to image sequences.
+                  Turn off the extract_flag to disable the functionality.
+    '''
+    print("Pre-processing step 2: Converting videos from .webm to image sequences.")
+
+    if extract_flag:
+        with tqdm(total=100, file=sys.stdout) as pbar:
+            for raw_vid in raw_vids:
+                # create a folder for each video with it's unique ID
+                v_name = raw_vid.split("/")[-1].split(".")[0]
+                split = raw_vid.split("/")[-2]
+                os.system("mkdir -p {}/{}/{}".format(dest_dir, split, v_name))
+
+                # check if this folder is already extracted
+                if (not os.path.isfile("{}/{}/{}/image-001.png".format(dest_dir, split, v_name))):
+                    # run the ffmpeg software to extract the videos based on the fps provided
+                    if fps is not None:
+                        os.system("ffmpeg -framerate {} -i {} {}/{}/{}/image-%03d.png".format(
+                            fps, raw_vid, dest_dir, split, v_name))
+                    else:
+                        os.system("ffmpeg -i {} {}/{}/{}/image-%03d.png".format(
+                            raw_vid, dest_dir, split, v_name))
+
+                    print(raw_vid, " has been converted..")
+                else:
+                    pass
+                pbar.update(1)
+                sleep(1)
+    else:
+        pass
+
+
+def create_dataframe(vid_list,create_flag=True):
+    """
+    :input:
+    :return : pandas dataframe
+    Description : creates dataframe
+                  Turn off the create_flag to disable functionality
+    """
+    print("Pre-processing step 3 : Creating dataframe.")
+
+    if create_flag:
+        df = pd.DataFrame({"path": vid_list})
+        with tqdm(total=100, file=sys.stdout) as pbar:
+            for i, vid_path in enumerate(df['path']):
+                # read the first frame of the video
+                im = plt.imread(vid_path + "/image-001.png")
+
+                # add split information
+                df.loc[i, 'split'] = vid_path.split("/")[-2]
+
+                # add frame resolution information
+                df.loc[i, 'height'] = int(im.shape[0])
+                df.loc[i, 'width'] = int(im.shape[1])
+                df.loc[i, 'aspect_ratio'] = im.shape[0] / im.shape[1]
+                df.loc[i, 'num_of_frames'] = int(len(os.listdir(vid_path)))
+
+                # image statistics
+                arr = im.flatten()
+                df.loc[i, 'first_frame_mean'] = np.mean(arr)
+                df.loc[i, 'first_frame_std'] = np.std(arr)
+                df.loc[i, 'first_frame_min'] = np.min(arr)
+                df.loc[i, 'first_frame_max'] = np.max(arr)
+                pbar.update(1)
+                sleep(1)
+
+        # decide crop group (see dataset_smthsmth_analysis.ipynb point(2) for analysis)
+        df = df.drop(df[df.width < 300].index)
+        df['crop_group'] = 1
+        df.loc[df.width >= 420, 'crop_group'] = 2
+
+        # reject extreme frame lengths
+        #     df = df.drop(df[z<(z.mean()-3*z.std())].index)
+        #     df = df.drop(df[z<(z.mean()+3*z.std())].index)
+        return df
+
+    else:
+        pass
+
+
+def _chunks(l, n):
+    """
+    Input : l (dtype : int)
+            n (dtype : int)
+    Description : Yield successive n-sized chunks from l.
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
